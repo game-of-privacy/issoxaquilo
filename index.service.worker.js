c4e6ea57f88bec037a984f4ae50c8934dd56d366
EliasPacheco <63831841+EliasPacheco@users.noreply.github.com>
2023-05-09 17:05:02 -0300

    .

--- index.service.worker.js
+++ index.service.worker.js
@@ -8,7 +8,7 @@ const CACHE_PREFIX = "issoxaquilo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
 // Files that will be cached on load.
-const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.512x512.png","index.512x512.png"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 const CACHABLE_FILES = ["index.wasm","index.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHABLE_FILES);
