e37c9c5c398b4c982036c1817a19a44a102c3cf1
EliasPacheco <63831841+EliasPacheco@users.noreply.github.com>
2023-05-09 17:21:50 -0300

    icon c

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1683663333|9956964579";
+const CACHE_VERSION = "1683663677|10301229162";
 const CACHE_PREFIX = "issoxaquilo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
