ea5f4dcf18646d0ed6a7b234401f4337e2212cb4
EliasPacheco <63831841+EliasPacheco@users.noreply.github.com>
2023-05-18 16:56:03 -0300

    Acertivas + menu ajustes

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1684436305|724592185";
+const CACHE_VERSION = "1684439672|1902419534";
 const CACHE_PREFIX = "issoxaquilo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
