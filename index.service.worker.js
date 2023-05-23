4cff5da3208515636cdc97a77c1b481451e5f6b8
EliasPacheco <63831841+EliasPacheco@users.noreply.github.com>
2023-05-23 15:45:39 -0300

    fonte ranking

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1684439672|1902419534";
+const CACHE_VERSION = "1684867446|1567276987";
 const CACHE_PREFIX = "issoxaquilo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
